5f3ea43ac41a5c108261908001773778d67e0019
FortochFint <99564038+FortochFint@users.noreply.github.com>
2025-08-02 23:06:50 +0200

    ➳

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
-      ⇦
+      ➳ 
       <Link href="/" className="hover:underline">
-        Back to main
+        Back
       </Link>
       .
     </h2>
