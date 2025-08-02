b3e8fca2ec1cfefcace20470f036efcbe9a70d04
FortochFint <99564038+FortochFint@users.noreply.github.com>
2025-08-02 20:30:07 +0200

    post header in page.stx

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -25,12 +25,7 @@ export default async function Post(props: Params) {
       <Container>
         <Header />
         <article className="mb-32">
-          <PostHeader
-            title={post.title}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-          />
+
           <PostBody content={content} />
         </article>
       </Container>
